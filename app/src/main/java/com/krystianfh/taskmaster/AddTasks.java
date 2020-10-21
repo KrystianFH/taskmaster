8ba8b4c7b529bfe4429cf3aed14a6273f9876d42
KrystianFH <krystianfrancuz@gmail.com>
2020-10-21 15:41:29 -0700

    completed lab 26

--- app/src/main/java/com/krystianfh/taskmaster/AddTasks.java
+++ app/src/main/java/com/krystianfh/taskmaster/AddTasks.java
@@ -3,6 +3,10 @@ package com.krystianfh.taskmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddTasks extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class AddTasks extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_tasks2);
+
+
+
+        Button addATaskButton = AddTasks.this.findViewById(R.id.addATaskButton);
+        addATaskButton.setOnClickListener((view) -> {
+            EditText title = findViewById(R.id.editTitle);
+            System.out.println(title.getText().toString());
+            EditText body = findViewById(R.id.editBody);
+            System.out.println(body.getText().toString());
+
+            int duration = Toast.LENGTH_SHORT;
+            CharSequence text = "Submitted";
+
+            Toast toast = Toast.makeText(this, text, duration);
+            toast.show();
+
+        });
+
     }
+
+
 }
